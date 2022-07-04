e37661b6b1a7404c6a6f6b5f0387a5c16426b778
aadilkhalifa <aadilkhalifa@gmail.com>
2022-07-04 15:28:10 +0300

    added multiple languages

--- server/script.js
+++ server/script.js
@@ -1,6 +1,12 @@
 const express = require('express');
 const app = express();
 const cors = require("cors");
+const {format} = require('sql-formatter');
+const prettify = require('html-prettify');
+var pretty = require('pretty');
+var cssbeautify = require('cssbeautify');
+const jsonFormatter = require('json-string-formatter');
+const fmt2json = require('format-to-json');
 
 const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: false}));
@@ -18,13 +24,69 @@ function listening(){
     console.log('server running');
 }
 
-app.post('/',add);
-function add(req, res){
+app.post('/json',json);
+function json(req, res){
     // console.log(req.body);
-    res.send(beautifyJavaScript(req.body.code));
+    beautifyJSON(req.body.code, req.body).then(result => {
+        // console.log(result);
+        res.send({
+            'data': result
+        });
+    });
+    // console.log(result);
+    // res.send(result);
+}
+app.post('/css',css);
+function css(req, res){
+    res.send(beautifyCSS(req.body.code));
+}
+app.post('/html',html);
+function html(req, res){
+    res.send(beautifyHTML(req.body.code));
+}
+app.post('/sql',sql);
+function sql(req, res){
+    res.send(beautifySQL(req.body.code));
+}
+app.post('/js',js);
+function js(req, res){
+    console.log('here i am');
+    var result = beautifyJavaScript(req.body.code);
+    console.log(result);
+    res.send(result);
 }
 
-function beautifyJavaScript (source) {
-    const beautify = require('js-beautify').js_beautify
-    return beautify(source, {indent_size: 2})
+function beautifyJavaScript (source, options = {}) {
+    const beautify = require('js-beautify').js_beautify;
+    var res = beautify(source);
+    console.log(res);
+    return res;
    }
+function beautifySQL (source) {
+        var res = format(source);
+        return res;
+   }
+function beautifyHTML (source) {
+        var res = pretty(source);
+        return res;
+   }
+function beautifyCSS (source) {
+        var res = cssbeautify(source, {
+            indent: '  ',
+            // openbrace: 'separate-line',
+            // autosemicolon: true
+        });
+        return res;
+   }
+async function beautifyJSON (source, options={}) {
+        delete options.code;
+        var new_options = {
+            'indent' : parseInt(options.indent),
+            'expand' : (options.expand === 'true'),
+            'strict' : (options.strict === 'true'),
+            'escape' : (options.escape === 'true'),
+            'unscape' : (options.unscape === 'true'),
+        }
+        return fmt2json(source, new_options).then(res => res.result);
+        return res.result;
+   }    
--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -11,12 +11,35 @@ var qs = require('qs');
 const options = [
     { value: 'Javascript', label: 'Javascript' },
     { value: 'SQL', label: 'SQL' },
+    { value: 'HTML', label: 'HTML' },
+    { value: 'CSS', label: 'CSS' },
+    { value: 'JSON', label: 'JSON' },
   ];
+const truefalseoptions = [
+    { value: 'true', label: 'True' },
+    { value: 'false', label: 'False' },
+  ];
+const indentoptions = [
+    { value: '0', label: '0' },
+    { value: '2', label: '2' },
+    { value: '4', label: '4' },
+    { value: '6', label: '6' },
+    { value: '8', label: '8' },
+    { value: '10', label: '10' },
+  ];
+
+var json_options = {
+    'indent' : 2,
+    'expand' : true,
+    'strict' : false,
+    'escape' : false,
+    'unscape ' : false,
+}
 
 function Home() {
 
     const [selectedLanguage, setSelectedLanguage] = useState('Javascript');
-    const [code, setCode] = useState('');
+    const [code, setCode] = useState(placeholders[selectedLanguage]);
 
     function handleEditorChange(value, event) {
         setCode(value);
@@ -24,11 +47,17 @@ function Home() {
 
     function handleSubmit() {
         var data = qs.stringify({
-            'code': code
+            'code': code,
+            ...json_options,
           });
+        var route = 'js';
+        if(selectedLanguage==='SQL') route = 'sql';
+        if(selectedLanguage==='HTML') route = 'html';
+        if(selectedLanguage==='CSS') route = 'css';
+        if(selectedLanguage==='JSON') route = 'json';
           var config = {
             method: 'post',
-            url: 'http://localhost:8000/',
+            url: 'http://localhost:8000/' + route,
             mode: 'no-cors',
             headers: { 
               'Content-Type': 'application/x-www-form-urlencoded'
@@ -37,12 +66,15 @@ function Home() {
           };
         axios(config)
             .then(function (response) {
+                // console.log(response.data.data)
                 var res = JSON.stringify(response.data);
+                if(selectedLanguage === 'JSON') res = JSON.stringify(response.data.data);
                 res = res.slice(1, -1);
                 res = res.replace(/\\n/g, "\n");
                 res = res.replace(/\\t/g, "\t");
                 res = res.replace(/\\b/g, "\b");
                 res = res.replace(/\\r/g, "\r");
+                res = res.replace(/\\"/g, '\"');
                 setCode(res);
             })
             .catch(function (error) {
@@ -53,6 +85,9 @@ function Home() {
     // useEffect(() => {
     //     console.log(code);
     //   }, [code])
+    useEffect(() => {
+        setCode(placeholders[selectedLanguage]);
+      }, [selectedLanguage])
 
     return (
         <div className="home-div">
@@ -65,16 +100,38 @@ function Home() {
                     <Editor
                         height="100%"
                         width="100%"
-                        defaultLanguage="javascript"
-                        defaultValue="// some comment"
+                        defaultLanguage={selectedLanguage.toLowerCase()}
+                        // defaultLanguage="html"
+                        path={selectedLanguage.toLowerCase()}
+                        defaultValue={placeholders[selectedLanguage]}
                         theme="vs-dark"
                         onChange={handleEditorChange}
                         value={code}
                     />
                 </div>
                 <div className="footer">
-                    <span className="button2" onClick={()=>{setCode('// this is text a=b\nvar a=10,b=0;')}}>Sample</span>
-                    <span className="button2" onClick={()=>{setCode('var a=1,b= 2;')}}>Sample</span>
+                    <span className="button2" onClick={()=>{setCode('// this is text a=b\nvar a=10,b=0;')}}>Sample JS</span>
+                    <span className="button2" onClick={()=>{setCode('SELECT * FROM tbl')}}>Sample SQL</span>
+                    <span className="button2" onClick={()=>{setCode(`<section class="wrapper">
+    <ul>
+    <li
+    v-for="(item, i) in list"
+    :key="i"
+    >
+    <SomeVueComponent
+    size="1.5rem"
+    v-html="getIcon('tickIcon').html"
+    />
+    <span>{{ item }}</span>
+    </li>
+    </ul>
+    <a
+    class="some-link"
+    href="#"
+    >Link</a>
+    </section>`)}}>Sample HTML</span>
+    <span className="button2" onClick={()=>{setCode('menu{color:red} navigation{background-color:#333}')}}>Sample CSS</span>
+    <span className="button2" onClick={()=>{setCode(placeholders['JSON'])}}>Sample JSON</span>
                     <span className="button2">Copy</span>
                     <span className="button" onClick={handleSubmit}>Beautify</span>
                 </div>
@@ -86,7 +143,38 @@ function Home() {
                     <Select
                         options={options}
                         onChange={(values) => {setSelectedLanguage(values[0].value)}}
+                        value = {selectedLanguage}
+                        placeholder = {'Javascript'}
                     />
+                    {
+                        selectedLanguage === 'JSON' ? 
+                        <>
+                            <h3>Indent</h3>
+                            <Select
+                                options={indentoptions}
+                                onChange={(values) => {json_options = {...json_options, 'indent': values[0].value}}}
+                                placeholder = {'2'}
+                            />
+                            <h3>Expand</h3>
+                            <Select
+                                options={truefalseoptions}
+                                onChange={(values) => {json_options = {...json_options, 'expand': values[0].value}}}
+                                placeholder = {'True'}
+                            />
+                            <h3>Escape</h3>
+                            <Select
+                                options={truefalseoptions}
+                                onChange={(values) => {json_options = {...json_options, 'escape': values[0].value}}}
+                                placeholder = {'False'}
+                            />
+                            <h3>Unscape</h3>
+                            <Select
+                                options={truefalseoptions}
+                                onChange={(values) => {json_options = {...json_options, 'unscape': values[0].value}}}
+                                placeholder = {'False'}
+                            />
+                        </> : null
+                    }
                 </div>
             </div>
         </div>
@@ -94,3 +182,19 @@ function Home() {
 }
 export default Home
 
+
+const placeholders = {
+    'Javascript': '// some comment',
+    'SQL': 'SELECT * from table;',
+    'HTML': '<div><p>hello world</p></div>',
+    'CSS': 'menu{color:red} navigation{background-color:#333}',
+    'JSON': `{  "bool": true,
+    "short array": [1, 2, 3],
+    "long array": [
+      {"x": 1, "y": 2},
+      {"x": 2, "y": 1},
+      {"x": 1, "y": 1},
+      {"x": 2, "y": 2}
+    ]
+  }`,
+};
